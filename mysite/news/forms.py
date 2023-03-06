f9ff3c3d6a585cd8bed9fb45fcfa119cefa1f716
Ashot Abazyan <ashot.93@mail.ru>
2023-03-07 00:58:39 +0400

    Added model related form

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,15 +1,20 @@
 from django import forms
-from news.models import Category
+
+from news.models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Title',
-                            widget=forms.TextInput(attrs={'class': 'form-control'})
-                            )
-    content = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    is_published = forms.BooleanField(label='Published', initial=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(),
-                                      empty_label='Choose category',
-                                      label='Category',
-                                      widget=forms.Select(attrs={'class': 'form-control'})
-                                      )
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = [
+            'title',
+            'content',
+            'is_published',
+            'category'
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
