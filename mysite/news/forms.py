9a74ee605993f68432a460f4f59404a59db32a30
Ashot Abazyan <ashot.93@mail.ru>
2023-03-06 20:42:17 +0400

    Added validation and fixed forms

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -3,7 +3,13 @@ from news.models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Title',
+                            widget=forms.TextInput(attrs={'class': 'form-control'})
+                            )
+    content = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    is_published = forms.BooleanField(label='Published', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                      empty_label='Choose category',
+                                      label='Category',
+                                      widget=forms.Select(attrs={'class': 'form-control'})
+                                      )
