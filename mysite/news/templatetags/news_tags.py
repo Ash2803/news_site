d4983dee53196a6b21baec53753ee6a56e40a50d
Ashot Abazyan <ashot.93@mail.ru>
2023-03-06 19:52:58 +0400

    Added form for news

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -5,6 +5,12 @@ from news.models import Category
 register = template.Library()
 
 
-@register.simple_tag()
+@register.simple_tag(name='get_categories_list')
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('news/categories_list.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {'categories': categories}
