dbf118f7db92058341b9ed82ff4c8d891b0346fd
Ashot Abazyan <ashot.93@mail.ru>
2023-03-08 03:52:01 +0400

    Added DetailView and CreateView

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,28 +3,28 @@ from django.urls import reverse
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
-    content = models.TextField(verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    title = models.CharField(max_length=150, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Publication date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update date')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Photo', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Published')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category')
 
     def get_absolute_url(self):
-        return reverse('view_news', kwargs={'news_id': self.pk})
+        return reverse('view_news', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
         ordering = ['-created_at']
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Category name')
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'category_id': self.pk})
@@ -33,6 +33,6 @@ class Category(models.Model):
         return self.title
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
         ordering = ['title']
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
 
 from news.forms import NewsForm
 from news.models import News, Category
@@ -18,14 +18,6 @@ class HomeNews(ListView):
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
-# def index(request):
-#     news = News.objects.all()
-#     context = {
-#         'news': news,
-#         'title': 'Список новостей',
-#     }
-#     return render(request, template_name='news/index.html', context=context)
-
 
 class NewsByCategory(ListView):
     model = News
@@ -42,6 +34,16 @@ class NewsByCategory(ListView):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+
+
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = get_object_or_404(Category, category_id=category_id)
@@ -50,22 +52,3 @@ def get_category(request, category_id):
         'category': category
     }
     return render(request, template_name='news/category.html', context=context)
-
-
-def view_news(request, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    context = {
-        'news_item': news_item
-    }
-    return render(request, template_name='news/view_news.html', context=context)
-
-
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
