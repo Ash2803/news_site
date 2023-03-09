00f23cf6eacd99e0978364f6b686e2f1da7e9a4b
Ashot Abazyan <ashot.93@mail.ru>
2023-03-10 01:15:22 +0400

    Optimized SQL queries

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -9,7 +9,8 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Update date')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Photo', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Published')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category', related_name='news')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'pk': self.pk})
